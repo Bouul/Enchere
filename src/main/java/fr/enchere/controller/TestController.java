f1279535f12b721e02c676f8a323f5e4a0899460
Aurélien Flahaut <123742403+Tuahalf@users.noreply.github.com>
2025-04-14 16:20:48 +0200

    Ajout template login

--- src/main/java/fr/enchere/controller/TestController.java
+++ src/main/java/fr/enchere/controller/TestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("/test")
     public String home() {
         return "test";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
