404a5e4d8aae8863f38ed5d7a86ef81eb143b810
Bouul <clement.boulesteix2023@campus-eni.fr>
2025-04-24 11:45:05 +0200

    /index -> /enchere

--- src/main/java/fr/enchere/configuration/WebConfig.java
+++ src/main/java/fr/enchere/configuration/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/images/**")
-                .addResourceLocations("file:C:\\Users\\glandry2023\\Desktop\\Enchere\\src\\main\\resources\\static\\uploads\\");
+                .addResourceLocations("file:C:\\Users\\cboulesteix2023\\IdeaProjects\\Enchere\\src\\main\\resources\\static\\uploads");
     }
 }
